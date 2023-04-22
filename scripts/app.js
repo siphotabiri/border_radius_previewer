4350db761264675ea08d20011367351b92e61fcd
Siyavuya Mankanku <siphokwesi1@outlook.com>
2023-04-22 16:10:24 +0100

    event listener added to button element to set the border radius value from the input element's value.

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,9 @@
 /* Design a function which collects input data from a web-page and applies it to a border-radius css property  */
 
-function setBorderRadius(){
+let button = document.getElementById('button');
+
+button.addEventListener("click", function setBorderRadius(){
     let box = document.getElementById("border-box");
     let borderRadiusInput = document.getElementById("border-radius-input").value;
     box.style.borderRadius = borderRadiusInput; 
-}
-
-const button = document.getElementById('button');
-button.addEventListener('click', setBorderRadius());
+})
